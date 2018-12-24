6c3678b31da7f1b53851e45a02937f2e5fbc6b02
Samy <haffoudhisamy@gmail.com>
2018-12-24 18:55:01 +0100

    no message

---
+++ imperative.ml
@@ -0,0 +1,38 @@
+let imprime_chiffre () =
+  for i = 0 to 9 do
+    print_int i
+  done;
+  print_newline ();;
+
+let p = [|3; 2; 1|];;
+let q = make_vect 4 2;;
+(*  vect_lengh q;; *)
+(*  q.(0) <- 1 *)
+
+let imprime_monome coeff degre =
+  if degre = 0 then print_int coeff else
+  if coeff <> 0 then
+    begin
+      print_string " + ";
+      if coeff <> 1 then print_int coeff;
+      print_string "x";
+      if degre <> 1 then
+        begin print_string "^"; print_int degre end
+    end;;
+
+let imprime_polynome p =
+  for i = 0 to vect_lengh p - 1 do imprime_monome p.(i) i done;;
+
+let compteur = ref 0;;
+(* !compteur;; *)
+compteur := 2;;
+compteur := !compteur + 1
+
+let incremente c = c := !c + 1;;
+
+let fact n =
+  begin
+    let accu = ref 1 in
+    for i = 1 to n do accu := i * !accu done;
+    !accu
+  end;;
