b8de7300749b76badd45f1ecd887479e03f3dcfc
Samy <haffoudhisamy@gmail.com>
2019-02-26 22:28:05 +0100

    fin TD 3

--- TD3.ml
+++ TD3.ml
@@ -65,3 +65,51 @@ let rec eliminate a l = match l with
 let rec purge l = match l with
   |[] -> []
   |t::q -> t::(eliminate t (purge q));;
+
+(* ensembles *)
+
+let rec appartient x l = match l with
+  |[] -> false
+  |t::q when t = x -> true
+  |t::q -> appartient x q;;
+
+let rec union l1 l2 = match l1, l2 with
+  |[], a -> a
+  |t::q, a when (appartient t a) = false -> t::(union q a)
+  |t::q, a -> union q a;;
+
+let rec intersection l1 l2 = match l1, l2 with
+  |[], a -> []
+  |t::q, a when appartient t a -> t::(intersection q a)
+  |t::q, a -> intersection q a;;
+
+let rec concatenation l1 l2 = match l1, l2 with
+  |[], a -> a
+  |t::q, a -> t::(concatenation q a);;
+
+(* insertion sort *)
+
+let rec insere x l = match l with
+  |[] -> [x]
+  |t::q when x >= t -> t::(x::q)
+  |t::q -> insere x q;;
+
+let rec insertion l = match l with
+  |[] -> []
+  |t::q -> insere t (insertion q);;
+
+(* selection sort *)
+
+let rec minimun l = match l with
+  |[] -> failwith "liste vide"
+  |[a] -> a
+  |t::q -> min t (minimun q);;
+
+let rec select m l = match l with
+  |[] -> []
+  |t::q when t = m -> select m q
+  |t::q -> t::select m q;;
+
+let rec selection l = match l with
+  |[] -> []
+  |a -> (minimun l)::(selection (select (minimun l) l));;
