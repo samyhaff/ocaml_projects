5cf5ffc06ca063e19a4332ace9d4b24ba715373e
Samy <haffoudhisamy@gmail.com>
2019-03-13 20:34:23 +0100

    no message

--- TD3.ml
+++ TD3.ml
@@ -11,6 +11,8 @@ let queue liste = match liste with
   |[] -> failwith "liste vide"
   |t::q -> q;;
 
+(* exercice 1 *)
+
 let rec keme liste k = match liste, k with
   |[], k -> failwith "pas de k-eme element"
   |t::q, 1 -> t
@@ -21,6 +23,48 @@ let rec avant_dernier l = match l with
   |[_] -> failwith "liste de longueur 1"
   |[a; b] -> a
   |_::q -> avant_dernier q;;
+
+let indice liste x =
+  let rec indiceAux liste k x = match liste, k with
+    |[], k -> failwith "élement absent de la liste..."
+    |t::q, k when t = x -> k
+    |t::q, k -> indiceAux q (k + 1) x
+  in indiceAux liste 0 x;;
+
+let inverse liste =
+  let rec inverseAux liste acc = match liste with
+    |[] -> acc
+    |t::q -> inverseAux q (t::acc)
+  in inverseAux liste [];;
+
+let maMap f liste =
+  let rec maMapAux f liste acc = match liste with
+    |[] -> acc
+    |t::q -> maMapAux f q ((f t)::acc)
+  in maMapAux f liste [];;
+
+(* exercice 2 *)
+
+let premier liste = match liste with
+  |[] -> failwith "liste vide..."
+  |t::q -> t;;
+
+let somme liste =
+  let rec sommeAux liste acc = match liste with
+    |[] -> acc
+    |t::q -> sommeAux q (t + acc)
+  in sommeAux liste 0;;
+(* ou *)
+let rec somme liste = match liste with
+  |[] -> failwith "liste vide"
+  |[a] -> a
+  |t::q -> t + somme(q);;
+
+let rec dernierElement liste =
+  premier (inverse liste);;
+
+let rec ajouterEnQueue liste x =
+  inverse (x::(inverse liste));;
 
 let rec mystere f liste = match liste with
   |[] -> []
@@ -37,6 +81,8 @@ let rec vrai f = function
   |[] -> []
   |t::q when f t = true -> t::(vrai f q)
   |t::q -> vrai f q;;
+
+(*  exercice 5 *)
 
 let rec concatenation l1 l2 = match l1 with
   |[] -> l2
@@ -112,4 +158,4 @@ let rec select m l = match l with
 
 let rec selection l = match l with
   |[] -> []
-  |a -> (minimun l)::(selection (select (minimun l) l));; 
+  |a -> (minimun l)::(selection (select (minimun l) l));;
