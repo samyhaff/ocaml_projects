380f88cbaa0b63410103384a8e4b462f32b5f25b
Samy <haffoudhisamy@gmail.com>
2019-03-30 19:18:36 +0100

    no message

--- revisionDS1.ml
+++ revisionDS1.ml
@@ -30,3 +30,26 @@ type entierNaturel = Zero | Successeur of entierNaturel;;
 
 type 'a monome == int * 'a;;
 type 'a polynome == ('a monome) list;;
+
+(* DS d'entrainement *)
+
+let rec unMystere l = match l with
+  |[] -> [], []
+  |(a, b)::q -> let q1, q2 = unMystere q in (a::q1, b::q2);;
+
+let nb0cc t ob i j =
+  let rec nbOccAux t ob i j c x =
+    if x = j then c
+    else if t.(x) = ob then nbOccAux t ob i j (c + 1) (x + 1)
+    else nbOccAux t ob i j c (x + 1)
+  in nbOccAux t ob i j 0 i;;
+
+type mot == int list;;
+
+let luka l =
+  let rec lukaAux l acc = match l with
+    |[] -> (acc = -1)
+    |t::q -> acc >= 0 && (lukaAux q (acc + t))
+  in lukaAux l 0;;
+
+let rec permutation t = 
